bba161d686503cf15862e65a73b1e586bd167fad
bhagyarajg <bhagyaraj.gaddam@gmail.com>
2018-02-03 11:11:33 -0500

    Feat: Implement data grid of Faas funtions

--- app/solution/solution.component.ts
+++ app/solution/solution.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FaasPlatformService } from '../impl/faasPlatform.service';
 
 @Component({
    moduleId: __moduleName,
    selector: 'my-solution',
    templateUrl: 'solution.component.html'
 })
-export class SolutionComponent { }
+export class SolutionComponent {
+   fnIds: string[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
+   faasInfoById: any[] = [];
+
+   constructor(private faasService: FaasPlatformService) {
+      for (let i = 0; i < this.fnIds.length; i++) {
+         faasService.getFaasInfo$(this.fnIds[i])
+            .subscribe(
+            info => { console.log(info); this.faasInfoById.push(info); });
+      }
+   }
+
+
+}
