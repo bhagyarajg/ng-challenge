405a6a51800b4b3a6abed6b1fb56fe2885a77722
bhagyarajg <bhagyaraj.gaddam@gmail.com>
2018-02-03 18:24:09 -0500

    Feat: Implement Alert funtionality

--- app/solution/solution.component.ts
+++ app/solution/solution.component.ts
@@ -4,42 +4,31 @@ import { Observable } from 'rxjs/Observable';
 import { IFaasUsage } from '../api/FaasUsage';
 
 @Component({
-   moduleId: __moduleName,
-   selector: 'my-solution',
-   templateUrl: 'solution.component.html'
+      moduleId: __moduleName,
+      selector: 'my-solution',
+      styleUrls: ['./solution.component.css'],
+      templateUrl: 'solution.component.html'
 })
 export class SolutionComponent {
-   fnIds: string[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
-   faasInfoById: any[] = [];
-   faasUsageById: Observable<IFaasUsage>;
-
-   constructor(private faasService: FaasPlatformService) {
-      for (let i = 0; i < this.fnIds.length; i++) {
-         faasService.getFaasInfo$(this.fnIds[i])
-            .subscribe(
-            info => { // console.log(info);
-               this.faasInfoById.push(info);
+      readonly amberThreshold: number = 1e7;
+      readonly redThreshold: number = 2e7;
+      fnIds: string[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
+      faasInfoById: any[] = [];
+      faasUsageById: Observable<IFaasUsage>;
+      constructor(private faasService: FaasPlatformService) {
+            for (let i = 0; i < this.fnIds.length; i++) {
+                  faasService.getFaasInfo$(this.fnIds[i])
+                        .subscribe(info => this.faasInfoById.push(info));
+            }
+            this.faasInfoById.forEach(element => {
+                  faasService.getFaasUsage$(element.id)
+                        .subscribe(usage => { element.usage = usage; });
             });
-
-         //     this.faasUsageById = faasService.getFaasUsage$(this.fnIds[i]);
-         /*   .subscribe(
-            usage => { //console.log(usage);
-               this.faasInfoById.push(usage); }
-            );*/
-
       }
-      this.faasInfoById.forEach(element => {
-         //  console.log(element.id);
-         //   console.log(faasService.getFaasUsage$(element.id));
-         //element.usage =
-         faasService.getFaasUsage$(element.id)
-            .subscribe(usage => { element.usage = usage; }
-            );
-         console.log(element);
-      });
-      //  console.log(this.faasInfoById);
-
-   }
-
-
+      amberAlert(size: number): boolean {
+            return (this.amberThreshold < size) && (size < this.redThreshold);
+      }
+      redAlert(size: number): boolean {
+            return size > this.redThreshold;
+      }
 }
