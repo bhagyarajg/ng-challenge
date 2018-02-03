2983c4c80c4fa0dfcd00d4b18f9bcccd3380705f
bhagyarajg <bhagyaraj.gaddam@gmail.com>
2018-02-03 13:31:20 -0500

    feat : Calculate total memory Allocation ands show state

--- app/solution/solution.component.ts
+++ app/solution/solution.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FaasPlatformService } from '../impl/faasPlatform.service';
+import { Observable } from 'rxjs/Observable';
+import { IFaasUsage } from '../api/FaasUsage';
 
 @Component({
    moduleId: __moduleName,
@@ -9,13 +11,34 @@ import { FaasPlatformService } from '../impl/faasPlatform.service';
 export class SolutionComponent {
    fnIds: string[] = ['1', '2', '3', '4', '5', '6', '7', '8'];
    faasInfoById: any[] = [];
+   faasUsageById: Observable<IFaasUsage>;
 
    constructor(private faasService: FaasPlatformService) {
       for (let i = 0; i < this.fnIds.length; i++) {
          faasService.getFaasInfo$(this.fnIds[i])
             .subscribe(
-            info => { console.log(info); this.faasInfoById.push(info); });
+            info => { // console.log(info);
+               this.faasInfoById.push(info);
+            });
+
+         //     this.faasUsageById = faasService.getFaasUsage$(this.fnIds[i]);
+         /*   .subscribe(
+            usage => { //console.log(usage);
+               this.faasInfoById.push(usage); }
+            );*/
+
       }
+      this.faasInfoById.forEach(element => {
+         //  console.log(element.id);
+         //   console.log(faasService.getFaasUsage$(element.id));
+         //element.usage =
+         faasService.getFaasUsage$(element.id)
+            .subscribe(usage => { element.usage = usage; }
+            );
+         console.log(element);
+      });
+      //  console.log(this.faasInfoById);
+
    }
 
 
